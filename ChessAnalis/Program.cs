ad8f9447d9c56be716eb4fb147d96e17be91990c
mishafominih <mishagjxn@mail.ru>
2020-10-11 10:29:52 +0500

    залил то что было

---
+++ ChessAnalis/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace chassAnaliz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("я считаю, жди)");
+            var res = Game.Update(true, new Map(new string[]
+            {
+                "   K    ",
+                "r       ",
+                "        ",
+                "        ",
+                "  k     ",
+                "        ",
+                "  q     ",
+                "       R"
+            }), 1);
+            while (true)
+            {
+                Console.Clear();
+                if (res == Result.win) Console.WriteLine("победа!!!");
+                if (res == Result.neytral) Console.WriteLine("ничья");
+                if (res == Result.lose) Console.WriteLine("проигрыш(((");
+                Console.Read();
+            }
+        }
+    }
+}
